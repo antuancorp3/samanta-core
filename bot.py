7a253d84069178006a086fc20ce36685a6d792e8
antuancorp3 <antuan.corp3@gmail.com>
2026-02-08 09:33:22 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import os
+print(">>> Iniciando Samanta con Hugging Face integrado <<<")import os
 import requests
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
